81c4cb5f842a22b9751a606ec2fc5b79a34f7d7f
Unah <unah@unah.local>
2020-06-23 22:41:16 +0300

    add-zoom-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZoomComponent } from './zoom/zoom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZoomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/zoom/zoom.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+
+
+import { ZoomMtg } from '@zoomus/websdk';
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+
+
+@Component({
+  selector: 'app-zoom',
+  templateUrl: './zoom.component.html',
+  styleUrls: ['./zoom.component.scss']
+})
+export class ZoomComponent implements OnInit {
+   // setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
+   signatureEndpoint = ''
+   apiKey = '6I8c_5CSTj21PCHz8GnhOw'
+   meetingNumber = 123456789
+   role = 0
+   leaveUrl = 'http://localhost:4200'
+   userName = 'Sifuma'
+   userEmail = ''
+   passWord = ''
+ 
+   constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
+ 
+   }
+ 
+   ngOnInit() {
+ 
+   }
+ 
+   getSignature() {
+     this.httpClient.post(this.signatureEndpoint, {
+       meetingNumber: this.meetingNumber,
+       role: this.role
+     }).toPromise().then((data: any) => {
+       if(data.signature) {
+         console.log(data.signature)
+         this.startMeeting(data.signature)
+       } else {
+         console.log(data)
+       }
+     }).catch((error) => {
+       console.log(error)
+     })
+   }
+ 
+   startMeeting(signature) {
+ 
+     document.getElementById('zmmtg-root').style.display = 'block'
+ 
+     ZoomMtg.init({
+       leaveUrl: this.leaveUrl,
+       debug: true, //optional
+       showMeetingHeader: false, //option
+       disableInvite: false, //optional
+       disableCallOut: false, //optional
+       disableRecord: false, //optional
+       disableJoinAudio: false, //optional
+       audioPanelAlwaysOpen: true, //optional
+       showPureSharingContent: false, //optional
+       isSupportAV: true, //optional,
+       isSupportChat: true, //optional,
+       isSupportQA: true, //optional,
+       isSupportCC: true, //optional,
+       screenShare: true, //optional,
+       rwcBackup: '', //optional,
+       videoDrag: true, //optional,
+       sharingMode: 'both', //optional,
+       videoHeader: true, //optional,
+       isLockBottom: true, // optional,
+       isSupportNonverbal: true, // optional,
+       isShowJoiningErrorDialog: true, // optional,
+       inviteUrlFormat: '', // optional
+       loginWindow: {  // optional,
+          width: 400,
+          height: 380
+         },
+       success: (success) => {
+         console.log(success)
+ 
+         ZoomMtg.join({
+           signature: "Nkk4Y181Q1NUajIxUENIejhHbmhPdy4xMjM0NTY3ODkuMTU5MjkyNTkzMzE3Mi4wLlN1ZmJRNVF5M2RPS3V2cFVYRjlJUCtFbHRoeGJvRGFtOTRnVi9BVmIxVFk9",
+           meetingNumber: this.meetingNumber,
+           userName: this.userName,
+           apiKey: this.apiKey,
+           userEmail: this.userEmail,
+           passWord: this.passWord,
+           success: (success) => {
+             console.log(success)
+           },
+           error: (error) => {
+             console.log(error)
+           }
+         })
+ 
+       },
+       error: (error) => {
+         console.log(error)
+       }
+     })
+   }
+
+}
