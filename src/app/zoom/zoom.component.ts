caaa491263f3c0135c7a5487b83567e129c34165
Unah <unah@unah.local>
2020-06-25 00:08:11 +0300

    fix-jquery-error

--- src/app/zoom/zoom.component.ts
+++ src/app/zoom/zoom.component.ts
@@ -16,14 +16,15 @@ ZoomMtg.prepareJssdk();
 })
 export class ZoomComponent implements OnInit {
    // setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
-   signatureEndpoint = ''
-   apiKey = '6I8c_5CSTj21PCHz8GnhOw'
-   meetingNumber = 123456789
-   role = 0
-   leaveUrl = 'http://localhost:4200'
-   userName = 'Unah'
-   userEmail = ''
-   passWord = ''
+  signatureEndpoint = 'https://unah-zoom.herokuapp.com'
+  apiKey = '6I8c_5CSTj21PCHz8GnhOw'
+  meetingNumber = 93836305128
+  role = 0
+  leaveUrl = 'http://localhost:4200'
+  userName = 'Sifuma'
+  userEmail = 'grace.sifuma@vitafluence.ai'
+  passWord = 'Pombebangi123'
+   
  
    constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
  
@@ -38,6 +39,7 @@ export class ZoomComponent implements OnInit {
        meetingNumber: this.meetingNumber,
        role: this.role
      }).toPromise().then((data: any) => {
+       console.log('......',data);
        if(data.signature) {
          console.log(data.signature)
          this.startMeeting(data.signature)
@@ -54,37 +56,13 @@ export class ZoomComponent implements OnInit {
      document.getElementById('zmmtg-root').style.display = 'block'
  
      ZoomMtg.init({
-       leaveUrl: this.leaveUrl,
-       debug: true, //optional
-       showMeetingHeader: false, //option
-       disableInvite: false, //optional
-       disableCallOut: false, //optional
-       disableRecord: false, //optional
-       disableJoinAudio: false, //optional
-       audioPanelAlwaysOpen: true, //optional
-       showPureSharingContent: false, //optional
-       isSupportAV: true, //optional,
-       isSupportChat: true, //optional,
-       isSupportQA: true, //optional,
-       isSupportCC: true, //optional,
-       screenShare: true, //optional,
-       rwcBackup: '', //optional,
-       videoDrag: true, //optional,
-       sharingMode: 'both', //optional,
-       videoHeader: true, //optional,
-       isLockBottom: true, // optional,
-       isSupportNonverbal: true, // optional,
-       isShowJoiningErrorDialog: true, // optional,
-       inviteUrlFormat: '', // optional
-       loginWindow: {  // optional,
-          width: 400,
-          height: 380
-         },
+      leaveUrl: this.leaveUrl,
+      isSupportAV: true,
        success: (success) => {
          console.log(success)
  
          ZoomMtg.join({
-           signature: "Nkk4Y181Q1NUajIxUENIejhHbmhPdy4xMjM0NTY3ODkuMTU5MjkyNTkzMzE3Mi4wLlN1ZmJRNVF5M2RPS3V2cFVYRjlJUCtFbHRoeGJvRGFtOTRnVi9BVmIxVFk9",
+           signature: signature,
            meetingNumber: this.meetingNumber,
            userName: this.userName,
            apiKey: this.apiKey,
